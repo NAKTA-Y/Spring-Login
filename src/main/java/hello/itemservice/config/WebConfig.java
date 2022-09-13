362d679fe525fe815b7234b64c6b3358e1d33041
nakta <nakta0511@gmail.com>
2022-09-13 17:18:51 +0900

    log interceptor & login check interceptor

--- src/main/java/hello/itemservice/config/WebConfig.java
+++ src/main/java/hello/itemservice/config/WebConfig.java
@@ -2,36 +2,56 @@ package hello.itemservice.config;
 
 import hello.itemservice.web.filter.LogFilter;
 import hello.itemservice.web.filter.LoginCheckFilter;
+import hello.itemservice.web.interceptor.LogInterceptor;
+import hello.itemservice.web.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public FilterRegistrationBean logFilter() {
+//    @Bean
+//    public FilterRegistrationBean logFilter() {
+//
+//        FilterRegistrationBean<Filter> registrationBean = new FilterRegistrationBean<>();
+//
+//        registrationBean.setFilter(new LogFilter());
+//        registrationBean.setOrder(1);
+//        registrationBean.addUrlPatterns("/*");
+//
+//        return registrationBean;
+//    }
+//
+//    @Bean
+//    public FilterRegistrationBean loginCheckFilter() {
+//
+//        FilterRegistrationBean<Filter> registrationBean = new FilterRegistrationBean<>();
+//
+//        registrationBean.setFilter(new LoginCheckFilter());
+//        registrationBean.setOrder(2);
+//        registrationBean.addUrlPatterns("/*");
+//
+//        return registrationBean;
+//    }
 
-        FilterRegistrationBean<Filter> registrationBean = new FilterRegistrationBean<>();
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
 
-        registrationBean.setFilter(new LogFilter());
-        registrationBean.setOrder(1);
-        registrationBean.addUrlPatterns("/*");
-
-        return registrationBean;
-    }
-
-    @Bean
-    public FilterRegistrationBean loginCheckFilter() {
-
-        FilterRegistrationBean<Filter> registrationBean = new FilterRegistrationBean<>();
-
-        registrationBean.setFilter(new LoginCheckFilter());
-        registrationBean.setOrder(2);
-        registrationBean.addUrlPatterns("/*");
-
-        return registrationBean;
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/", "/members/add", "/login", "logout",
+                        "/css/**", "/*.ico", "/error"
+                );
     }
 }
